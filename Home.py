00709d006597a1951673d19cd32c75b522753820
GunavathyShanmuganathan <121713100+GunavathyShanmuganathan@users.noreply.github.com>
2023-05-02 18:36:17 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title("Industrial Copper Modeling")
 from PIL import Image
-
-image = Image.open(r'C:\Users\User\img.webp')
-
+image = Image.open(r'C:\Users\User\cop.png')
 st.image(image,width=600)
+st.write("The copper industry deals with less complex data related to sales and pricing. However, this data may suffer from issues such as skewness and noisy data, which can affect the accuracy of manual predictions. Dealing with these challenges manually can be time-consuming and may not result in optimal pricing decisions. A machine learning regression model can address these issues by utilizing advanced techniques such as data normalization, feature scaling, and outlier detection, and leveraging algorithms that are robust to skewed and noisy data.This web application will save time on data analysis and improves strategic decision making." 
+        )
